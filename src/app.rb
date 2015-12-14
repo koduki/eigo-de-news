700143be43430bb50ec0226e94ec6420aaf88b37
koduki <you@pascal.orz.cn>
2015-12-14 20:01:04 +0900

    modify json name

--- src/app.rb
+++ src/app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require './models.rb'
 
-get '/' do
+get '/contents.json' do
   newsList = NewsList.new
   newsList.load
   x = newsList.data["http://mashable.com/2015/12/10/google-photos-shared-albums/#rmnTre_AZEqH"]
