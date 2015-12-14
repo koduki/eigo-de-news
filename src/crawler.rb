815e542fa66644bdbf79fe870a58f00728d70792
koduki <you@pascal.orz.cn>
2015-12-14 20:03:21 +0900

    modify some bugs.

--- src/crawler.rb
+++ src/crawler.rb
@@ -1,26 +1,22 @@
+require 'mechanize'
 require './models.rb'
 
 class Crawler
-	def initialize
-		@newsList = NewsList.new
-		@agent = Mechanize.new
-		@agent.follow_meta_refresh = true
-		@agent.user_agent_alias = 'Windows IE 9'
-	end
+  def initialize
+    @newsList = NewsList.new
+    @agent = Mechanize.new
+    @agent.follow_meta_refresh = true
+    @agent.user_agent_alias = 'Windows IE 9'
+  end
 
-def crawl
-	url = "http://mashable.com/2015/12/10/google-photos-shared-albums/#rmnTre_AZEqH"
+  def crawl
+    url = "http://mashable.com/2015/12/10/google-photos-shared-albums/#rmnTre_AZEqH"
+    html = @agent.get(url)
 
-	require 'mechanize'
+    title = html.title
+    contents = html.at("section.article-content").search(".//p").map{|x| "<p>#{x.text}</p>" }.join
 
-
-
-	html = agent.get(url)
-
-	title = html.title
-	contents = html.at("section.article-content").search(".//p").map{|x| "<p>#{x.text}</p>" }.join
-
-	@newsList.add url, News.new(title, contents)
-	@newsList.store
-end
+    @newsList.add url, News.new(title, contents)
+    @newsList.store
+  end
 end
