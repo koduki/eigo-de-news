4dd7fd501524a28af3059889296771e0a5ddb88f
koduki <koduki@nebula>
2015-12-25 08:32:58 +0900

    Fixed wpm calculation

--- src/public/js/histories.js
+++ src/public/js/histories.js
@@ -1,24 +1,20 @@
 $(function(){
-	const wpm = 150;
 	const readCoentens = function(){
 		console.log("start: read contents.");
 		$.getJSON("histories", {}, function(res){
 			const items = res.map(function(x) {
 				console.log(x.title);
-				
+
 				const time = x.time;
-				const minute = ("0" + parseInt(time / 60)).slice(-2);
-				const seconed = ( "0" + parseInt(time % 60)).slice(-2);
-				
-				const wpm = parseInt(time / x.new_words) * 60
-				
+				const wpm_record = parseInt((x.new_words/ x.time) * 60);
+
 				const url = "./contents.html?page=" + x.news_id;
-				const text = x.news_title + "(<span>wpm:" + wpm + "</span>)";
-				
+				const text = x.news_title + "(<span>wpm:" + wpm_record + "</span>)";
+
 				const d = new Date(x.timestamp);
 				var timestamp = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + " ";
 				timestamp += d.getHours() + ":" + d.getMinutes();
-				
+
 				const li = $("<li>").append($("<span>").text(timestamp + "\t"));
 				const a = $("<a>").attr("href", url).html(text);
 				return li.append(a);
